282faf2303866a3fded578bf0642b3449a1a42c7
Kamil Szwaradzki <kamil.szwaradzki@gmail.com>
2025-08-17 21:45:02 +0200

    Finish section: Authentication and posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+    def index
+        @posts = Post.all
+        if user_signed_in?
+            @user = current_user
+        end
+    end
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
